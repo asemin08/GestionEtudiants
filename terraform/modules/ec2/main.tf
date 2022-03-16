376e7fd18b2f2f50f8666a79ded36d0e3d683337
asemin08 <semin.allan@gmail.com>
2022-03-16 16:06:33 +0100

    Ajout terraform

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,41 @@
+data "aws_ami" "aws_ami" {
+  most_recent = true
+  owners      = ["${var.id_compte_ami}"]
+
+  filter {
+    name   = "name"
+    values = ["${var.nom_ami}"]
+  }
+}
+
+resource "aws_instance" "projet-ec2" {
+  ami               = data.aws_ami.aws_ami.id
+  instance_type     = var.type_instance
+  key_name          = var.cle_ssh
+  availability_zone = var.zone_dispo
+  security_groups   = ["${var.securite_groupe}"]
+  tags = {
+    Name = "${var.ec2_name}-ec2"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y git",
+      "sudo amazon-linux-extras install -y ansible2",
+      "git clone -b main https://github.com/${var.git_proprietaire}/${var.git_projet}.git",
+      "cd ${var.git_projet}/ansible/",
+      "ansible-galaxy install -r requirements.yml --force",
+      "ansible-playbook -i hosts.yml projet.yml"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "${var.utilisateur_ssh}"
+      private_key = file("../.aws/${var.cle_ssh}.pem")
+      host        = "${self.public_ip}"
+    }
+  }
+
+
+}
+
